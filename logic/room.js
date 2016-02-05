76e5fdb1797d7f9b8bf75ad6786bc99c3eb2c466
tongjinle <1328192478@qq.com>
2016-02-05 14:33:36 +0800

    完成room

---
+++ logic/room.js
@@ -0,0 +1,47 @@
+var _ = require('underscore');
+var Hall = (function(){
+	var cls = function() {
+		this.id = null;
+		this.roomIdList = [];
+		this.playerIdList = [];
+		this.status = null;
+		this.gameName = null;
+	};
+
+	var staticHandler = cls;
+	var publicHandler = cls.prototype;
+
+	// static
+	// private
+
+	// public
+	publicHandler.open = function(){
+		this.status = 'open';
+	};
+
+	publicHandler.close = function(){
+		this.status = 'close';
+	};
+
+	publicHandler.addRoom = function(roomId){
+		this.roomIdList.push(roomId);
+		this.roomIdList = _.uniq(this.roomIdList);
+	};
+
+	publicHandler.removeRoom = function(roomId){
+		this.roomIdList = _.without(this.roomIdList,function(n){return n == roomId;});
+	};
+
+	publicHandler.addPlayer = function(pId){
+		this.playerIdList.push(pId);
+		this.playerIdList = _.uniq(this.playerIdList);
+	};
+
+	publicHandler.removePlayer = function(pId){
+		this.playerIdList = _.without(this.playerIdList,function(n){ return n == pId;});
+	};
+
+	return cls;
+
+}).call(this);
+
