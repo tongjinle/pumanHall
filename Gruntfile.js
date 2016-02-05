65d0659c856e6f864bfd447385ef60fca104d785
tongjinle <1328192478@qq.com>
2016-02-05 23:14:09 +0800

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,27 +3,31 @@ module.exports = function(grunt) {
 	// // show elapsed time at the end  
 	// require('time-grunt')(grunt);  
 	// // load all grunt tasks  
-	// require('load-grunt-tasks')(grunt);  
+	require('load-grunt-tasks')(grunt);  
 	// grunt.loadNpmTasks('grunt-contrib-less');
 	// grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-jsdoc');
 	grunt.initConfig({
-		jsdoc: {
-			dist: {
-				src: ['logic/**/*.js'],
-				options: {
-					destination: 'doc'
-				}
-			}
+		jsdoc : {
+		    dist : {
+		        src: ['src/**/*.js', 'README.md'],
+		        options: {
+		            destination: 'doc',
+		            template : "node_modules/grunt-jsdoc/node_modules/ink-docstrap/template",
+		            configure : "node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json"
+		        }
+		    }
 		}
 	});
 
 
 
 	grunt.registerTask('serve', function() {
-		grunt.task.run([
-			'jsdoc'
-		]);
+		console.log('jsdoc start');
+		// grunt.task.run([
+		// 	'jsdoc'
+		// ]);
+		console.log('jsdoc end');
 	});
 
 	grunt.registerTask('default', ['serve']);
