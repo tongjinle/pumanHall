ade8490dc9556b6e40739967ad864d53f2f08fe0
tongjinle <1328192478@qq.com>
2016-03-08 23:55:41 +0800

    room 完整版

--- game-server/logic/room.js
+++ game-server/logic/room.js
@@ -1,14 +1,15 @@
 // 房间
-var GameFactory = require('./gameFactory');
 var _ = require('underscore');
+var Dict = require('./dict');
+var GameFactory = require('./gameFactory');
 
 var Room = (function(){
 	var cls = function(hallId,gameName) {
 		this.id = _.uniqueId();
-		this.status = CONF.ROOM_STATUS.OPEN;
+		this.status = 'waiting';
 		this.hallId = hallId;
-		this.gameName = null;
-		this.playerList = [];
+		this.gameName = gameName;
+		this.playerList = new Dict();
 		this.game = GameFactory.create(this.gameName);
 	};
 
@@ -21,17 +22,36 @@ var Room = (function(){
 
 	// public
 	// 修改状态
+	publicHandler.changeStatus = function(status){
+		var self = this;
+		self.status = status;
+	};
 
 	// 增加玩家
+	publicHandler.addPlayer = function(player){
+		var self = this;
+		return self.playerList.add(player.name,player);
+	};	
 
 	// 删除玩家
+	publicHandler.removePlayer = function(username){
+		var self = this;
+		return self.playerList.remove(username);
+	};
 
-	// 玩家修改状态
+	// 修改玩家状态
+	publicHandler.changePlayerStatus = function(username,status){
+		var self = this;
+		var p = self.playerList.get(username);
+		p.status = status;
+		return p;
+	};	
 
 
 	// 发送信息到游戏逻辑,并且从游戏逻辑获取处理结果
 	publicHandler.send = function(msg){
-		return this.game.accpet(msg);
+		var self = this;
+		return self.game.accpet(msg);
 	};
 
 
