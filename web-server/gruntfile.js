85675037b1b54535a91c6f63a2a5b8bf9fc03fbb
tongjinle <1328192478@qq.com>
2016-03-10 23:35:24 +0800

    修改web端的结构,配置了grunt

---
+++ web-server/gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function (grunt) {  
+	require("colors");
+	// show elapsed time at the end  
+	require('time-grunt')(grunt);  
+	// load all grunt tasks  
+	require('load-grunt-tasks')(grunt);  
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	// grunt.loadNpmTasks('grunt-jsdoc');
+
+	grunt.initConfig({  
+		watch: {  
+			livereload: {  
+				options: {  
+					livereload: '<%= connect.options.livereload %>'    
+				},  
+				files: [
+					'public/**/*.html',
+					'public/css/bundle.css',
+					'public/js/**/*.js', 
+				]  
+			},
+			less:{
+				files:[
+					'public/less/**/*.less'
+				],
+				tasks:["less"]
+			}
+		},
+		less:{
+			dev:{
+				files:{
+					// "css/bundle.css":["less/**/*.less","directive/less/**/*.less"]
+					'public/css/bundle.css':'<%= watch.less.files %>'
+				}
+			}
+		},
+		connect: {  
+			options: {  
+				port: 9001,  
+				livereload: 35731,  
+				// change this to '0.0.0.0' to access the server from outside  
+				hostname: 'localhost'  
+			},  
+			livereload: {  
+				options: {  
+					open: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/public/index.html',  
+				}  
+			}
+		}
+	});  
+
+
+	grunt.registerTask('compile',function(){
+		grunt.task.run([
+			'less'
+		]);
+	});
+  
+	grunt.registerTask('serve', function () {  
+		grunt.task.run([  
+			'compile',  
+			'connect:livereload',  
+			'watch'
+		]);  
+	});  
+
+  
+	grunt.registerTask('default', ['serve']);  
+};  
